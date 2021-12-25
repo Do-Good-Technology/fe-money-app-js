e814957b69d4ff7702b9ffd04f79e85c53064d86
Baariq Fairuuz Azhar <baariq.fairuuz@dataon.com>
2021-12-25 23:11:00 +0700

    slicing register page

--- src/pages/auth/register/Register.js
+++ src/pages/auth/register/Register.js
@@ -1,34 +1,48 @@
-import { Col, Image, Row, Form as AntdForm } from 'antd'
+import { Col, Image, Row, Form as AntdForm, Typography } from 'antd'
 
+import RegisterForm from '../../../components/auth/register/RegisterForm';
 import LogoIcon from '../../../assets/icons/logo.svg';
+import { Link } from 'react-router-dom';
+
+const { Text, Link: AntdLink } = Typography
 
 const Logo = () => (
     <Image alt='' src={LogoIcon} width="50vw" preview={false} />
 )
 
-const Form = () => {
-    return (
-        // <Form
-        //     name="register"
-
-        // >
-
-        // </Form>
-        <></>
-    )
-}
+const ToLogin = () => (
+    <Text>Have an Account ? {' '}
+        <Link to="/login">
+            <AntdLink>Log in</AntdLink>
+        </Link>
+    </Text>
+)
 
 //? Todo
 // Register
 // _ Logo
-// _ Form
+// _ RegisterForm
+// _ ToLogin have account
 export default function Register() {
     return (
-        <Row justify="center" style={{ paddingTop: '100px' }}>
-            <Col span={12}>
-                <Logo />
+        <Row justify="center" gutter={[0, 18]} style={{ paddingTop: '64px' }}>
+            <Col span={24}>
+                <Row justify='center'>
+                    <Col>
+                        <Logo />
+                    </Col>
+                </Row>
             </Col>
-
+            <Col span={24}>
+                <RegisterForm />
+                <Row justify='center'>
+                    <Col >
+                        <ToLogin />
+                    </Col>
+                </Row>
+            </Col>
+            <Col span={24}>
+            </Col>
         </Row>
     )
 }
